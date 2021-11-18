7ca3d9213436ae55027e282af289722480f9d075
menna15 <mennaahmedali77@gmail.com>
2021-11-18 17:39:14 +0200

    script1

---
+++ Test Scripts/Script1.spec.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+// (describe) function is used to hold a collection of tests 
+// and (it) function represents an individual test
+describe("login",function(){
+    it("sign in",function(){
+
+        cy.visit('https://www.tumblr.com/')
+        cy.get('.Fygd5 .Z8Ux2').contains('Log in').click({force: true})
+        cy.get('input[name="email"]').type("mennaahmedali77@gmail.com").should("be.visible")
+        cy.get('input[name="password"]').type("66332111menna").should("be.visible")
+        cy.get('.EvhBA').contains('Log in').click({force: true})
+        
+    })
+    
+    it("Add Quote",function(){
+
+        
+
+        
+    })
+    // it("Create Text post",function(){
+
+        
+    //     cy.get('.FOqaP').contains('Text').click({force: true})
+    //     cy.waitFor(5000)
+    //     cy.get('.control').click({force: true})
+    //     // cy.get('editor').find('p').type("This post was created by a test script ")
+    //     // // cy.get('.root').find('p').type("This post was created by a test script ")
+    //     // cy.get('.button-area').contains('Post').click()
+
+        
+    // })
+
+})
