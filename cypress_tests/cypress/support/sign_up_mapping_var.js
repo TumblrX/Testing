c6aa9d148a1c2bba8ed4eb935c2a7d3174ee9cf8
menna15 <mennaahmedali77@gmail.com>
2021-11-20 14:34:30 +0200

    functional documentation

--- cypress_tests/cypress/support/sign_up_mapping_var.js
+++ cypress_tests/cypress/support/sign_up_mapping_var.js
@@ -1,12 +1,39 @@
-/** @const */const BEGIN_SIGN_UP = '.Fygd5 .Z8Ux2'
-/** @const */const INPUT_EMAIL = 'input[name="email"]'
-/** @const */const INPUT_PASSWORD = 'input[name="password"]'
-/** @const */const INPUT_BLOGNAME = 'input[name="blogName"]'
-/** @const */const INPUT_AGE = 'input[name="age"]'
-/** @const */const SIGN_UP_ACTION = '.TRX6J .EvhBA'
-/** @const */const NEXT_ACTION = '.Fygd5 .TRX6J'
-/** @const */const ERROR_MSG_FIRST = '.Fygd5 .oFCPF'
-/** @const */const ERROR_MSG_SECOND = '.Fygd5 .oFCPF'
+/** 
+ * first div that contain two buttons, second sign up botton
+ * @const BEGIN_SIGN_UP */
+const BEGIN_SIGN_UP = '.Fygd5 .Z8Ux2'
+/** 
+ * inside input tag in sign up form
+ * @const INPUT_EMAIL  */
+const INPUT_EMAIL = 'input[name="email"]'
+/** 
+ * inside input tag in sign up form
+ * @const INPUT_PASSWORD */
+const INPUT_PASSWORD = 'input[name="password"]'
+/** 
+ * inside input tag in sign up form
+ * @const INPUT_BLOGNAME */
+const INPUT_BLOGNAME = 'input[name="blogName"]'
+/** 
+ * inside input tag in sign up form
+ * @const INPUT_AGE */
+const INPUT_AGE = 'input[name="age"]'
+/**
+ * first div that contain form, second button
+ *  @const SIGN_UP_ACTION */
+const SIGN_UP_ACTION = '.TRX6J .EvhBA'
+/** 
+ * first div that contain form, second button
+ * @const NEXT_ACTION */
+const NEXT_ACTION = '.Fygd5 .TRX6J'
+/** 
+ * first div that contain form, second div contain error message
+ * @const ERROR_MSG_FIRST */
+const ERROR_MSG_FIRST = '.Fygd5 .oFCPF'
+/** 
+ * first div that contain form, second div contain error message
+ * @const ERROR_MSG_SECOND */
+const ERROR_MSG_SECOND = '.Fygd5 .oFCPF'
 
 /** @const */const VARIABLES = {
     BEGIN_SIGN_UP: BEGIN_SIGN_UP, INPUT_EMAIL: INPUT_EMAIL,
