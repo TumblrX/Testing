7264cc8b93057db672cdb7a0c0721be033e5aa2a
menna15 <mennaahmedali77@gmail.com>
2021-12-09 12:29:18 +0200

    Integration folder added

---
+++ Integration/cypress/support/sign_up_mapping_var.js
@@ -0,0 +1,44 @@
+/** 
+ * first div that contain two buttons, second sign up botton
+ * @const BEGIN_SIGN_UP */
+const BEGIN_SIGN_UP = '.Fygd5 .Z8Ux2'
+/** 
+ * inside input tag in sign up form
+ * @const INPUT_EMAIL  */
+const INPUT_EMAIL = 'input[name="email"]'
+/** 
+ * inside input tag in sign up form
+ * @const INPUT_PASSWORD */
+const INPUT_PASSWORD = 'input[name="password"]'
+/** 
+ * inside input tag in sign up form
+ * @const INPUT_BLOGNAME */
+const INPUT_BLOGNAME = 'input[name="blogName"]'
+/** 
+ * inside input tag in sign up form
+ * @const INPUT_AGE */
+const INPUT_AGE = 'input[name="age"]'
+/**
+ * first div that contain form, second button
+ *  @const SIGN_UP_ACTION */
+const SIGN_UP_ACTION = '.TRX6J .EvhBA'
+/** 
+ * first div that contain form, second button
+ * @const NEXT_ACTION */
+const NEXT_ACTION = '.Fygd5 .TRX6J'
+/** 
+ * first div that contain form, second div contain error message
+ * @const ERROR_MSG_FIRST */
+const ERROR_MSG_FIRST = '.Fygd5 .oFCPF'
+/** 
+ * first div that contain form, second div contain error message
+ * @const ERROR_MSG_SECOND */
+const ERROR_MSG_SECOND = '.Fygd5 .oFCPF'
+
+/** @const */const VARIABLES = {
+    BEGIN_SIGN_UP: BEGIN_SIGN_UP, INPUT_EMAIL: INPUT_EMAIL,
+    INPUT_PASSWORD: INPUT_PASSWORD, INPUT_BLOGNAME: INPUT_BLOGNAME, INPUT_AGE: INPUT_AGE,
+    SIGN_UP_ACTION: SIGN_UP_ACTION, NEXT_ACTION: NEXT_ACTION, ERROR_MSG_FIRST: ERROR_MSG_FIRST,
+    ERROR_MSG_SECOND: ERROR_MSG_SECOND
+}
+module.exports = { VARIABLES }
