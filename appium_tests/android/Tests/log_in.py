c6bdfded3c130c8c6ecbdd6c204cdfdc60c9a2cd
nada elsayed <49396399+nadaelsayed11@users.noreply.github.com>
2021-12-09 19:21:47 +0200

    android tests

---
+++ appium_tests/android/Tests/log_in.py
@@ -0,0 +1,7 @@
+from support import log_in_func
+
+log_in_func.invaled_email("nadaelsayed@ex", "That email doesn't have a Tumblr account. Sign up now?")
+# log_in_func.invaled_email("", )
+log_in_func.false_password("1111", "Incorrect email address or password. Please try again")
+# log_in_func.working_log_in("nadaelsayed@gmail.com", "nadaelsayed147258369")
+
