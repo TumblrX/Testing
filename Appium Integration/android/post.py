3a8ae4f3fa49073922b68ecea5a471c5f888dcab
Menna Ahmad <mennaahmedali77@gmail.com>
2022-01-01 23:27:53 +0200

    Update post.py

--- Appium Integration/android/post.py
+++ Appium Integration/android/post.py
@@ -2,5 +2,17 @@ from support import post_func
 from support import log_in_func
 import unittest
 
+class TestPost(unittest.TestCase):
+    def test_0(self):
+        self.assertTrue(login("mennaahmedali77@gmail.com","1234567menna"))
+    def test_1(self):
+        self.assertTrue(post_func.create_post("by a test script2"))
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
 
 
